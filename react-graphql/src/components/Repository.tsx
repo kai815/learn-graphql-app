fe678a895c4a95f90c7211c4701d6371df0bdf1f
kai815 <h.kaizuka815@gmail.com>
2023-01-27 10:17:42 +0900

    :necktie: 一覧を取得

--- react-graphql/src/components/Repository.tsx
+++ react-graphql/src/components/Repository.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client';
 
+const SEARCH_REPOSITORY = gql`
+    query {
+        search(type: REPOSITORY, query: "user:kai815", last: 100) {
+            repositoryCount
+            nodes {
+                ... on Repository {
+                    id
+                    url
+                    name
+                    description
+                    createdAt
+                }
+            }
+        }
+    }
+`;
 export const Repository = ()=> {
-  return <div>Repository</div>
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  return (
+    <div>
+      <h1>kai815のRepositoryたち</h1>
+      <ul>
+      {data.search.nodes.map((repo:any,index:number)=>(<li key={`n-${index}`}>{repo.name}</li>))}
+      </ul>
+    </div>
+  )
 }

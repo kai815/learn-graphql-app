576c75de96690a6f25d2117b4622607091567d07
kai815 <h.kaizuka815@gmail.com>
2023-02-04 19:33:42 +0900

    ディレクトリの移動

--- src/components/photos/photos.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import {PhotosResolver} from "./photo.resolver";
-import {PhotoService} from "./photo.service";
-
-@Module({
-  providers:[PhotosResolver,PhotoService]
-})
-export class PhotosModule {}

34ef214bbce62361204c69cd44469f8af378536b
kai815 <h.kaizuka815@gmail.com>
2023-02-12 18:50:48 +0900

    :heavy_plus_sign: users.moduleでphotoのdb接続のモジュールを使えるように修正

--- backend-nestjs/src/components/users/users.module.ts
+++ backend-nestjs/src/components/users/users.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import {UsersResolver} from "./user.resolver";
 import {UserService} from "./user.service";
 import {PhotoService} from "@/components/photos/photo.service";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Photo, PhotoSchema} from "@/components/photos/schemas/photo.schema";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Photo.name, schema: PhotoSchema }])], //userがphotoseriviceを使うに当たり、db接続のmoduleを提供しないといけないので
   providers:[UsersResolver,UserService,PhotoService]
 })
 export class UsersModule {}

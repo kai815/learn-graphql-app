11759f1fff2cad1df72d62e8adb7dc68311bf9fb
kai815 <h.kaizuka815@gmail.com>
2023-02-12 18:47:01 +0900

    :label: categoryの追加

--- backend-nestjs/src/components/photos/dto/createPhoto.dto.ts
+++ backend-nestjs/src/components/photos/dto/createPhoto.dto.ts
@@ -1,4 +1,5 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { PhotoCategory } from "@/components/photos/interfaces/photo.model";
 
 @InputType()
 export class CreatePhotoDto {
@@ -7,6 +8,9 @@ export class CreatePhotoDto {
 
   @Field((type) => String)
   name: string;
+
+  @Field((type) => PhotoCategory)
+  category: PhotoCategory;
 
   @Field((type) => String,{ nullable: true })
   description?: string;

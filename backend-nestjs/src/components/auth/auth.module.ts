03bcc494d0e33fcdce4e4cd484c2df9d4d5bd94e
kai815 <h.kaizuka815@gmail.com>
2023-02-12 19:33:43 +0900

    :necktie: githubauthした後にdbに追加

--- backend-nestjs/src/components/auth/auth.module.ts
+++ backend-nestjs/src/components/auth/auth.module.ts
@@ -3,9 +3,18 @@ import {AuthResolver} from '@/components/auth/auth.resolver'
 import {AuthService} from '@/components/auth/auth.service'
 import {UserService} from "@/components/users/user.service";
 import {HttpModule} from "@nestjs/axios";
+import {MongooseModule} from "@nestjs/mongoose";
+import {User, UserSchema} from "@/components/users/schemas/user.schema";
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature(
+      [
+        { name:User.name, schema: UserSchema }
+      ]
+    )
+  ],
   providers:[AuthResolver,UserService,AuthService]
 })
 export class AuthModule {}

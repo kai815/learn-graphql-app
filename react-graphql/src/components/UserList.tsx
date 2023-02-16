e509f3cd71cf544a664b4c9733965345429fbfd0
kai815 <h.kaizuka815@gmail.com>
2023-02-17 08:16:12 +0900

    :necktie: addfakeuserを追加

--- react-graphql/src/components/UserList.tsx
+++ react-graphql/src/components/UserList.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useQuery, gql } from '@apollo/client';
+import React, {useState} from 'react'
+import {useQuery, gql, useMutation} from '@apollo/client';
 import {UserModel} from "../generated";
 
 
@@ -17,11 +17,28 @@ const AllUSER = gql`
         }
     }
 `;
+
+const ADD_FAKE_USERS_MUTATION = gql`
+    mutation addFakeUsers {
+        addFakeUsers(count:2) {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
 export const UserList = ()=> {
   const { loading, error, data } = useQuery(AllUSER);
-  console.log({data})
+
+  const [addFakeUsers,{loading:updating,error:mutateError,data:mutatedata}] = useMutation(ADD_FAKE_USERS_MUTATION)
+  const [count,setCount] = useState(0)
+
   if (loading) return <p>Loading...</p>;
+  if (updating) return <p>Updating...</p>;
   if (error) return <p>Error : {error.message}</p>;
+  if (mutateError) return <p>mutateError : {mutateError.message}</p>;
+
+  console.log({mutatedata})
   return (
     <div>
       <h1>ユーザー一覧</h1>
@@ -30,6 +47,18 @@ export const UserList = ()=> {
           <li key={`n-${index}`}>{user.githubLogin}</li>
         ))}
       </ul>
+        <input type="number" name="count" value={count} onChange={(e)=>{
+          const count = e.target.value
+          setCount(Number(count))
+        }}/>
+        <button type="submit" onClick={()=>{
+          console.log({variables:{count:count}})
+          addFakeUsers().then(result=>{
+            console.log({result})
+          })
+        }}>
+          add Fake Users
+        </button>
     </div>
   )
 }

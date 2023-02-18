a20413822ecc48c848798a9e047163cf99517796
kai815 <h.kaizuka815@gmail.com>
2023-02-18 16:03:28 +0900

    :necktie: submitでmutate

--- react-graphql/src/components/UserList.tsx
+++ react-graphql/src/components/UserList.tsx
@@ -19,8 +19,8 @@ const AllUSER = gql`
 `;
 
 const ADD_FAKE_USERS_MUTATION = gql`
-    mutation addFakeUsers {
-        addFakeUsers(count:2) {
+    mutation addFakeUsers($count:Float!) {
+        addFakeUsers(count:$count) {
             githubLogin
             name
             avatar
@@ -28,16 +28,24 @@ const ADD_FAKE_USERS_MUTATION = gql`
     }
 `
 export const UserList = ()=> {
-  const { loading, error, data,refetch } = useQuery(AllUSER);
+  const { loading, error, data, refetch } = useQuery(AllUSER);
 
   const [addFakeUsers,{loading:updating,error:mutateError,data:mutatedata}] = useMutation(ADD_FAKE_USERS_MUTATION)
-  const [count,setCount] = useState(0)
-
+  const onSubmit = async (e:React.SyntheticEvent)=>{
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      count: { value: string };
+    };
+    const countValue = target.count.value;
+    const count = Number(countValue)
+    console.log({count})
+    await addFakeUsers({variables:{count}})
+    await refetch()
+  }
   if (loading) return <p>Loading...</p>;
   if (updating) return <p>Updating...</p>;
   if (error) return <p>Error : {error.message}</p>;
   if (mutateError) return <p>mutateError : {mutateError.message}</p>;
-
   console.log({mutatedata})
   return (
     <div>
@@ -47,19 +55,12 @@ export const UserList = ()=> {
           <li key={`n-${index}`}>{user.githubLogin}</li>
         ))}
       </ul>
-        <input type="number" name="count" value={count} onChange={(e)=>{
-          const count = e.target.value
-          setCount(Number(count))
-        }}/>
-        <button type="submit" onClick={()=>{
-          console.log({variables:{count:count}})
-          addFakeUsers().then(result=>{
-            console.log({result})
-            refetch()
-          })
-        }}>
+      <form method="post" onSubmit={onSubmit}>
+        <input type="number" name="count"/>
+        <button type="submit">
           add Fake Users
         </button>
+      </form>
     </div>
   )
 }

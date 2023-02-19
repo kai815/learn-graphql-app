0aaea3924ff7087eb3e7bad90c81c875cfba12f1
kai815 <h.kaizuka815@gmail.com>
2023-02-19 16:11:12 +0900

    :necktie: 認証した後にmeを再取得

--- react-graphql/src/components/AuthorizeUser.tsx
+++ react-graphql/src/components/AuthorizeUser.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useQuery, gql,useMutation } from '@apollo/client';
+import {useSearchParams} from "react-router-dom";
 
 const ME = gql`
   query getMe{
@@ -25,19 +26,24 @@ const AUTH_GITHUB_USER = gql`
 
 
 export const AuthorizeUser = ()=>{
-  const {loading,data,error} = useQuery(ME)
+  const {loading, data, error, refetch} = useQuery(ME)
+  const [_searchParams, setSearchParams] = useSearchParams();
+
   const [githubAuthMutation,{loading:authing,data:authData,error:authError}] = useMutation(AUTH_GITHUB_USER)
   const requestCode = () => {
     const clientID = import.meta.env.VITE_CLIENT_ID
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`
   }
-  if (window.location.search.match(/code=/)) {
-    const code = window.location.search.replace("?code=", "")
-    console.log(code)
-    githubAuthMutation({ variables: { code } }).then(response=>{
-      localStorage.setItem('token', response.data.githubAuth.token)
-    })
-  }
+  useEffect(()=>{
+    if (window.location.search.match(/code=/)) {
+      const code = window.location.search.replace("?code=", "")
+      githubAuthMutation({ variables: { code } }).then( async (response)=>{
+        localStorage.setItem('token', response.data.gitHubAuth.token)
+        await refetch()
+      })
+      setSearchParams({});
+    }
+  },[])
   if (loading) return <p>CurrentUserLoading...</p>;
   if(error) return <p>login error...</p>
   if(data?.me?.githubLogin){
